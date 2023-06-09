d525e495f5b22931fb88cf849ac679d2c35d6065
Gabriel <gabriel113@web.de>
2023-06-09 11:59:57 +0200

    - updated AccessController (RequestMapping, GetMapping) accepts url to increment customer score number - updated AccessService (incrementCustomerScore)

--- src/main/java/com/gripsaves/ConnectionCMS2/control/AccessController.java
+++ src/main/java/com/gripsaves/ConnectionCMS2/control/AccessController.java
@@ -1,6 +1,7 @@
 package com.gripsaves.ConnectionCMS2.control;
 
 import com.gripsaves.ConnectionCMS2.access.Customer;
+import com.gripsaves.ConnectionCMS2.service.AccessService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,31 @@ import java.util.List;
 @RequestMapping(path = "api/access/customer")
 public class AccessController {
 
-    @GetMapping
+    private final AccessService accessService;
+    private final Customer customer;
+        public AccessController(AccessService accessService, Customer customer) {
+        this.customer = customer;
+        this.accessService = accessService;
+    }
+
+    @GetMapping ("/list")
     public List<Customer> accessPrint(){
         return List.of(
                 new Customer("Julia", 18, 2)
         );
+    }
+
+    @GetMapping ("/increment")
+    public int incrementCustomerScore(){
+        System.out.println("AccessController: Current customer name: " + customer.getName());
+        System.out.println("AccessController: Current customer score: " + customer.getScore());
+        int customerScoreNumberToIncrement = customer.getScore();
+        System.out.println("AccessController: Current local int customerScoreToIncrement: " + customerScoreNumberToIncrement);
+        customerScoreNumberToIncrement = accessService.incrementCustomerScore(customerScoreNumberToIncrement);
+        System.out.println("AccessController: Current local int customerScoreToIncrement (after inc.): " + customerScoreNumberToIncrement);
+        System.out.println("AccessController: Current customer name: " + customer.getName());
+        System.out.println("AccessController: Current customer score: " + customer.getScore());
+
+        return customerScoreNumberToIncrement;
     }
 }
--- src/main/java/com/gripsaves/ConnectionCMS2/service/AccessService.java
+++ src/main/java/com/gripsaves/ConnectionCMS2/service/AccessService.java
@@ -1,9 +1,20 @@
 package com.gripsaves.ConnectionCMS2.service;
 
+import com.gripsaves.ConnectionCMS2.access.Customer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccessService {
 
+    private final Customer customer;
 
+    public AccessService(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int incrementCustomerScore(int customerScoreNumberToIncrement) {
+        int customerScoreToIncrement = customer.getScore();
+        customer.setScore(customerScoreNumberToIncrement + 1);
+        return customer.getScore();
+    }
 }
